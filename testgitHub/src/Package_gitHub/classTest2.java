57312189e9430a4009c0caeb9defbd94ff0cd012
balaji <balaji@DESKTOP-7Q3O31M>
2019-10-03 16:21:37 +0530

    update 4

--- testgitHub/src/Package_gitHub/classTest2.java
+++ testgitHub/src/Package_gitHub/classTest2.java
@@ -2,7 +2,7 @@ package Package_gitHub;
 
 public class classTest2 {
 	public static void main(String[] args) {
-		System.out.println("This is main class");
+		System.out.println("This is main class...update");
 		
 		classTest2 t1=new classTest2();
 		t1.test();
@@ -10,7 +10,7 @@ public class classTest2 {
 	}
 	void test()
 	{
-		System.out.println("we are in method");
+		System.out.println("we are in method....update");
 	}
 	
 

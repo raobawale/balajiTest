30fd9a84c105867aefc079f411b4ff5c743c5602
unknown <deshmukhshubhada912@gmail.com>
2025-10-04 09:27:04 +0530

    method colling is added

---
+++ testgitHub/src/shubhada/methodCalling/src/task1/A.java
@@ -0,0 +1,18 @@
+package task1;
+
+public class A {
+	public void m1() {
+		System.out.println("m1 method");
+
+	}
+
+	public void m2() {
+
+		System.out.println("m2 method");
+	}
+	public static void main(String[] args) {
+		A a= new A();
+		a.m1();
+		a.m2();
+	}
+}
